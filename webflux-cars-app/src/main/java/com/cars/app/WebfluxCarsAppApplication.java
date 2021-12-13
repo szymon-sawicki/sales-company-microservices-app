ca22f01393c2f5051d65d49972ba21df51414147
Szymon Sawicki <szymonsawick@gmail.com>
2021-12-13 22:29:48 +0100

    changes

---
+++ webflux-cars-app/src/main/java/com/cars/app/WebfluxCarsAppApplication.java
@@ -0,0 +1,13 @@
+package com.cars.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxCarsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxCarsAppApplication.class, args);
+    }
+
+}
---
+++ webflux-cars-app/src/test/java/com/cars/app/WebfluxCarsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCarsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
